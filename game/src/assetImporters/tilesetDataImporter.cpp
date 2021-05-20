88d7899708737a63ae4db81f8239c09e248b45ae
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2021-05-20 23:43:44 +0200

    Added support for comments in assets

--- game/src/assetImporters/tilesetDataImporter.cpp
+++ game/src/assetImporters/tilesetDataImporter.cpp
@@ -4,18 +4,18 @@
 #include <sstream>
 #include <fstream>
 
+#include <regex>
+
 template<>
 static void wok::res::create(const std::string& name, std::shared_ptr<TilesetData>& asset)
 {
 	std::string tilesetPath = (std::stringstream()
-		<< "assets/" << name << ".json"
+		<< "assets/" << name << ".jsonc"
 		).str();
 
-	nlohmann::json data;
-	{
-		std::ifstream levelFile(tilesetPath);
-		levelFile >> data;
-	}
+	std::ifstream levelFile(tilesetPath);
+
+	nlohmann::json data = nlohmann::json::parse(levelFile, nullptr, true, true);
 
 	asset->path = data["path"];
 	asset->tileSize = data["tile_size"];
